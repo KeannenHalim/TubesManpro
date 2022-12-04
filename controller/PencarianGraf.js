13ef7ead607e989c6942df967fab51fcb5dc4a82
edofarrell <edo.farrell17@gmail.com>
2022-12-04 14:35:02 +0700

    Add query on Pencarian Graf

--- controller/PencarianGraf.js
+++ controller/PencarianGraf.js
@@ -1,9 +1,34 @@
 import express from "express";
+import { db } from "./database.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("PencarianGraf");
+});
+
+router.get("/cariGraf", async (req, res) => {
+  const queryString =
+    "SELECT source, target, weight FROM interaction WHERE book = '?' ORDER BY weight DESC LIMIT 10";
+  // const book = req.body.something;
+
+  const query = (conn, queryString, book) => {
+    return new Promise((resolve, reject) => {
+      conn.query(queryString, book, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
+
+  const conn = await db();
+  const result = await query(conn, queryString, book);
+  conn.release();
+
+  res.send(result);
 });
 
 export { router as pencarianGraf };
